523289e4ec0596b80ffa2ab7440f3cffcc484cf3
aaron <aaronmlucia@gmail.com>
2021-12-10 19:04:53 -0500

    draft step six handleClick

--- connect4.js
+++ connect4.js
@@ -9,6 +9,7 @@ const WIDTH = 7;
 const HEIGHT = 6;
 
 let currPlayer = 1; // active player: 1 or 2
+let piecesPlayed = 0;
 const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
@@ -67,19 +68,22 @@ function placeInTable(y, x) {
   let newPiece = document.createElement('div');
   newPiece.classList.add('piece', `p${currPlayer}`);
   //added p before the currPlayer number in the class to avoid requiring unicode characters to express the numbers in the CSS, which makes it difficult to read/understand
+
   let spot = document.querySelector(`#${CSS.escape(y + "-" + x)}`);
   spot.append(newPiece);
-  if (currPlayer === 1) {
-    currPlayer++;
-  } else {
-    currPlayer--;
-  }
+  
+  //marks the spot as played on the JS board
+  board[y][x] = currPlayer;
+
+  //counts as a piece played (for alternate tie logic - see handleClick below)
+  // piecesPlayed++;
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
   // TODO: pop up alert message
+  alert(msg);
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -94,6 +98,9 @@ function handleClick(evt) {
     return;
   }
 
+
+  // let htmlBoard = document.getElementById('board');
+
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
   placeInTable(y, x);
@@ -101,13 +108,20 @@ function handleClick(evt) {
   // check for win
   if (checkForWin()) {
     return endGame(`Player ${currPlayer} won!`);
-  }
+  };
 
   // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
+  // is every element of every array true?
+  if (board.every(column => column.every(cell => { cell === true }))) {
+    return endGame("The board is full with no winner. Tied game!")
+  }
+  // OR: is the board full based on its height and width?
+  // if (piecesPlayed >= HEIGHT * WIDTH) {
+  //   return endGame('The board is full with no winner. The game results in a tie!')
+  // };
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  currPlayer === 1 ? currPlayer = 2 : currPlayer = 1;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -130,12 +144,12 @@ function checkForWin() {
 
   // TODO: read and understand this code. Add comments to help you.
 
-  for (let y = 0; y < HEIGHT; y++) {
-    for (let x = 0; x < WIDTH; x++) {
-      let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      let vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      let diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      let diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+  for (let y = 0; y < HEIGHT; y++) { //for each row
+    for (let x = 0; x < WIDTH; x++) { //for each cell in each row
+      let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]]; //check for horizontal win
+      let vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]]; //check for vertical win
+      let diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]]; //check for diagonal win to the upper right
+      let diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]]; //check for diagonal win to the lower left
 
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
